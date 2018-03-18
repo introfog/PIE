8f957a7796cad4eb325ec231a52a05f0d6e8c8eb
introfog <chubrickdm@gmail.com>
2018-03-18 20:44:50 +0300

    Work on engine was started.

--- src/com/introfog/Main.java
+++ src/com/introfog/Main.java
@@ -1,12 +1,12 @@
 package com.introfog;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("JustGame");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JFrame frame = new JFrame("Demo PIE");
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.setSize (800, 600);
 		frame.add(new Display ());
 		frame.setVisible(true);
