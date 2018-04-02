1d8a3b22e93e6b6921af17d078bf094393605443
introfog <chubrickdm@gmail.com>
2018-04-02 15:08:41 +0300

    Correct position was updated by variable BORDER_SLOP.

--- src/com/introfog/PIE/Manifold.java
+++ src/com/introfog/PIE/Manifold.java
@@ -2,11 +2,24 @@ package com.introfog.PIE;
 
 public class Manifold{
 	public final float CORRECT_POSITION_PERCENT = 0.8f;
+	public final float BORDER_SLOP = 0.02f;
 	
 	public float penetration;
 	public Vector2f normal;
 	public Body a;
 	public Body b;
+	
+	
+	private void initializeCollision (){
+		if (a.shape == Body.Shape.circle && b.shape == Body.Shape.circle){
+			Circle A = (Circle) a;
+			Circle B = (Circle) b;
+			
+			normal = Vector2f.sub (B.position, A.position);
+			normal.normalize ();
+			penetration = A.radius + B.radius - (float) Math.sqrt (Vector2f.distanceWithoutSqrt (B.position, A.position));
+		}
+	}
 	
 	
 	public Manifold (Body a, Body b){
@@ -24,42 +37,37 @@ public class Manifold{
 	}
 	
 	public void solve (){
-		if (a.shape == Body.Shape.circle && b.shape == Body.Shape.circle){
-			Circle A = (Circle) a;
-			Circle B = (Circle) b;
-			
-			normal = Vector2f.sub (B.position, A.position);
-			normal.normalize ();
-			penetration = A.radius + B.radius - (float) Math.sqrt (
-					Vector2f.distanceWithoutSqrt (B.position, A.position));
-			
-			// Вычисляем относительную скорость
-			Vector2f rv = Vector2f.sub (B.velocity, A.velocity);
-			
-			// Вычисляем относительную скорость относительно направления нормали
-			float velAlongNormal = Vector2f.dotProduct (rv, normal);
-			
-			// Не выполняем вычислений, если скорости разделены
-			if (velAlongNormal > 0){
-				return;
-			}
-			
-			// Вычисляем упругость
-			float e = Math.min (A.restitution, B.restitution);
-			
-			// Вычисляем скаляр импульса силы
-			float j = -(1 + e) * velAlongNormal;
-			j /= A.invertMass + B.invertMass;
-			
-			// Прикладываем импульс силы
-			
-			Vector2f impulse = Vector2f.mul (normal, j);
-			A.velocity.sub (Vector2f.mul (impulse, A.invertMass));
-			B.velocity.add (Vector2f.mul (impulse, B.invertMass));
+		initializeCollision ();
+		
+		// Вычисляем относительную скорость
+		Vector2f rv = Vector2f.sub (b.velocity, a.velocity);
+		
+		// Вычисляем относительную скорость относительно направления нормали
+		float velAlongNormal = Vector2f.dotProduct (rv, normal);
+		
+		// Не выполняем вычислений, если скорости разделены
+		if (velAlongNormal > 0){
+			return;
 		}
+		
+		// Вычисляем упругость
+		float e = Math.min (a.restitution, b.restitution);
+		
+		// Вычисляем скаляр импульса силы
+		float j = -(1 + e) * velAlongNormal;
+		j /= a.invertMass + b.invertMass;
+		
+		// Прикладываем импульс силы
+		Vector2f impulse = Vector2f.mul (normal, j);
+		a.velocity.sub (Vector2f.mul (impulse, a.invertMass));
+		b.velocity.add (Vector2f.mul (impulse, b.invertMass));
 	}
 	
 	public void correctPosition (){
+		if (penetration < BORDER_SLOP){
+			return;
+		}
+		
 		Vector2f correction = Vector2f.mul (normal, penetration * CORRECT_POSITION_PERCENT);
 		a.position.sub (Vector2f.mul (correction, a.invertMass));
 		b.position.add (Vector2f.mul (correction, b.invertMass));
